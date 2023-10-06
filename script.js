55b4e24dbe7d269f4e64641be828f0c59ca0cd2c
Saharsh Burse <saharsh2kjain@gmail.com>
2023-10-06 19:53:14 +0530

    Carousel Added

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ document.addEventListener("click", e => {
 
 
 
+
+
+const slidesContainer = document.getElementById("slideshow");
+const slide = document.querySelector(".slide");
+// const prevButton = document.getElementById("slide-arrow-prev");
+// const nextButton = document.getElementById("slide-arrow-next");
+
+// nextButton.addEventListener("click", () => {
+//   const slideWidth = slide.clientWidth;
+//   slidesContainer.scrollLeft += slideWidth;
+// });
+
+// prevButton.addEventListener("click", () => {
+//   const slideWidth = slide.clientWidth;
+//   slidesContainer.scrollLeft -= slideWidth;
+// });
+
+
+
+let i=1;
+let noofSlides=3;
+setInterval(function() {
+   const slideWidth = slide.clientWidth;
+
+slidesContainer.scrollLeft += slideWidth;
+   i++;
+   console.log("after",i);
+  if(i===noofSlides+1){
+    i=1;
+    // const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth*noofSlides;
+    
+    console.log("slide");
+  } 
+}, 5000);
+
+
+
+
+  
+  
